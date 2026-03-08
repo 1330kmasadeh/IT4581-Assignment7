9a38b0279a6e54b08d8b28870144bc63bdd34e4b
1330kmasadeh <masadeh423@gmail.com>
2026-03-08 05:11:36 -0500

    Update higher_order_functions.py

--- higher_order_functions.py
+++ higher_order_functions.py
@@ -1,18 +1,13 @@
-# higher_order_functions.py
-# Demonstrating higher-order functions: map, filter, reduce
 
 from functools import reduce
 
 numbers = [1, 2, 3, 4, 5, 6]
 
-# mapping example
 mapped = list(map(lambda x: x * 2, numbers))
 print("Map result:", mapped)
 
-# filtering example
 filtered = list(filter(lambda x: x % 2 == 0, numbers))
 print("Filter result:", filtered)
 
-# reducing example
 reduced = reduce(lambda x, y: x + y, numbers)
 print("Reduce result:", reduced)
